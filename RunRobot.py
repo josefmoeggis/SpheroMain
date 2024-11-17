6b451fdec69ba2b2a8633f22d162d984dae672a1
josefmoeggis <joeloe99@hotmail.com>
2024-11-17 22:01:49 +0100

    directory problem

--- RunRobot.py
+++ RunRobot.py
@@ -1,5 +1,9 @@
 import os
 import sys
+
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
 
 import asyncio
