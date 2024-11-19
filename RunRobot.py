252ba52da2ed7fb4c5d7fefd9cd921c85cda3641
josefmoeggis <joeloe99@hotmail.com>
2024-11-19 16:05:37 +0100

    Testing control and flexbuffers2

--- RunRobot.py
+++ RunRobot.py
@@ -18,51 +18,49 @@ time.sleep(2)
 
 def run_client():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.connect((HOST, PORT))
+        s.connect((HOST, PORT))
 
-            # Receive response
-            data = b''
-            while True:
-                chunk = s.recv(1024)
-                if not chunk:
-                    break
-                data += chunk
+        # Receive response
+        data = b''
+        while True:
+            chunk = s.recv(1024)
+            if not chunk:
+                break
+            data += chunk
 
-            # Unpack the response
-            try:
-                root = flex.GetRoot(data)
-                response_dict = root.Value
-                # Access value as a dictionary using Value property
-                left_mode = response_dict['leftMode']
-                left_speed = response_dict['leftSpeed']
-                right_mode = response_dict['rightMode']
-                right_speed = response_dict['rightSpeed']
-                servo1 = response_dict['cameraYaw']
-                servo2 = response_dict['cameraPitch']
-                heading_mode = response_dict['headingMode']
-                speed = response_dict['speed']
-                heading = response_dict['heading']
-                flags = response_dict['flags']
+        # Unpack the response
+        root = flex.GetRoot(data)
+        response_dict = root.Value
+        # Access value as a dictionary using Value property
+        left_mode = response_dict['leftMode']
+        left_speed = response_dict['leftSpeed']
+        right_mode = response_dict['rightMode']
+        right_speed = response_dict['rightSpeed']
+        servo1 = response_dict['cameraYaw']
+        servo2 = response_dict['cameraPitch']
+        heading_mode = response_dict['headingMode']
+        speed = response_dict['speed']
+        heading = response_dict['heading']
+        flags = response_dict['flags']
 
-                if heading_mode:
-                    rvr.drive_with_heading(speed, heading, flags)
-                else:
-                    rvr.raw_motors(
-                        left_mode=uint8(left_mode),
-                        left_duty_cycle=uint8(left_speed),  # Valid duty cycle range is 0-255
-                        right_mode=right_mode,
-                        right_duty_cycle=right_speed  # Valid duty cycle range is 0-255
-                    )
+        if heading_mode:
+            rvr.drive_with_heading(speed, heading, flags)
+        else:
+            rvr.raw_motors(
+                left_mode=uint8(left_mode),
+                left_duty_cycle=uint8(left_speed),  # Valid duty cycle range is 0-255
+                right_mode=right_mode,
+                right_duty_cycle=right_speed  # Valid duty cycle range is 0-255
+            )
 
-                print(f'Right: {right_speed}, Left: {left_speed}, servo1: {servo1}, servo2: {servo2}, left_mode: {left_mode}, right_mode: {right_mode}, headingMode: {heading_mode}, speed: {speed}, heading: {heading}')
+        print(f'Right: {right_speed}, Left: {left_speed}, servo1: {servo1}, servo2: {servo2}, left_mode: {left_mode}, right_mode: {right_mode}, headingMode: {heading_mode}, speed: {speed}, heading: {heading}')
 
-            except Exception as e:
-                print(f"Error unpacking response: {e}")
-                print(f"Raw received data: {data.hex()}")
-
-        except Exception as e:
+'''            except Exception as e:
             print(f"Error unpacking response: {e}")
+            print(f"Raw received data: {data.hex()}")
+
+    except Exception as e:
+            print(f"Error unpacking response: {e}")'''
 
 
 
