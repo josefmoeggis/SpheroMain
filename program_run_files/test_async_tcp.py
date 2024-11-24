28b8d13644e47428faaa576e97c53825d4bf4269
josefmoeggis <joeloe99@hotmail.com>
2024-11-24 21:22:04 +0100

    testing with acc9

--- program_run_files/test_async_tcp.py
+++ program_run_files/test_async_tcp.py
@@ -64,9 +64,9 @@ async def main():
 
     while True:
         try:
-            distance1, distance2, imu = await sensors(tof1, tof2, manager)
-            print(distance1, distance2, imu)
-            await com.run_tx_client(imu, imu, [distance1, distance2], HOST, PORT)
+            distance1, distance2, imu, acc = await sensors(tof1, tof2, manager)
+            print(distance1, distance2, imu, acc)
+            await com.run_tx_client(imu, acc, [distance1, distance2], HOST, PORT)
             await asyncio.sleep(0.1)
         except Exception as e:
             print(f"Error in main loop: {e}")
