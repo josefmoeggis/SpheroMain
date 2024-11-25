4801cbcaa132a5f82cb37b21b8def997aa74101f
josefmoeggis <joeloe99@hotmail.com>
2024-11-25 16:19:05 +0100

    trying to run a single function

--- program_run_files/test_async_tcp.py
+++ program_run_files/test_async_tcp.py
@@ -2,7 +2,6 @@ import os
 import sys
 import camera_sensors as camsen
 import TCP_flexbuffers as com
-import socket
 
 # USE THIS FILE AS BASE FOR MAIN IN FUTURE JOSEF
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../')))
@@ -13,7 +12,7 @@ from sphero_sdk import SerialAsyncDal
 from sphero_sdk import RvrStreamingServices
 
 HOST = "10.22.119.215"
-PORT = 9090
+PORT = 5001
 
 loop = asyncio.get_event_loop()
 
@@ -34,31 +33,17 @@ async def ToF_read(tof):
     except Exception as e:
         print(e)
 
-async def sensors_tx(tof1, tof2, manager):
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        try:
-            s.connect((HOST, PORT))
-            print('connected')
-            while True:
-
-                distance1, distance2, imu = await asyncio.gather(
-                    ToF_read(tof1),
-                    ToF_read(tof2),
-                    asyncio.to_thread(manager.get_latest_imu_data),
-                )
-                imu_rot_dict = imu['IMU']
-                imu_rot = [imu_rot_dict['Roll'], imu_rot_dict['Pitch'], imu_rot_dict['Yaw']]
-                imu_acc_dict = imu['Accelerometer']
-                imu_acc = [imu_acc_dict['X'], imu_acc_dict['Y'], imu_acc_dict['Z']]
-                print(imu_rot)
-                await com.run_tx_client(imu_rot, imu_acc, [distance1, distance2], s)
-                await asyncio.sleep(0.2)
-
-        except Exception as e:
-            print(f"Error in tx_client: {e}")
-
-        finally:
-            s.shutdown(socket.SHUT_WR)
+async def sensors(tof1, tof2, manager):
+    distance1, distance2, imu = await asyncio.gather(
+        ToF_read(tof1),
+        ToF_read(tof2),
+        asyncio.to_thread(manager.get_latest_imu_data),
+    )
+    imu_rot_dict = imu['IMU']
+    imu_rot = [imu_rot_dict['Roll'], imu_rot_dict['Pitch'], imu_rot_dict['Yaw']]
+    imu_acc_dict = imu['Accelerometer']
+    imu_acc = [imu_acc_dict['X'], imu_acc_dict['Y'], imu_acc_dict['Z']]
+    return distance1, distance2, imu_rot, imu_acc
 
 
 async def main():
@@ -82,13 +67,18 @@ async def main():
     # await cam.start()
 
     try:
-        print('i got here')
-        sensor_task = asyncio.create_task(sensors_tx(tof1, tof2, manager))
-        await sensor_task
-        print('i got here 2')
+        while True:
+            try:
+                distance1, distance2, imu, acc = await sensors(tof1, tof2, manager)
+                print(distance1, distance2, imu, acc)
+                await com.run_tx_client(imu, acc, [distance1, distance2], HOST, PORT)
+                await asyncio.sleep(0.1)
+            except Exception as e:
+                print(f"Error in main loop: {e}")
+                await asyncio.sleep(1)
+                continue
     except Exception as e:
-        print(f"Error in main loop: {e}")
-        await asyncio.sleep(1)
+        print(e)
 
 
 if __name__ == '__main__':
