174499d9adc65e9e778acdb8846d61c1c751eb9c
josefmoeggis <joeloe99@hotmail.com>
2024-11-26 16:15:38 +0100

    changing receive

--- program_run_files/TCP_flexbuffers.py
+++ program_run_files/TCP_flexbuffers.py
@@ -60,6 +60,7 @@ async def run_rx_client(rvr, host, port):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.connect((host, port))
+            s.setblocking(False)
             print('connected to socket')
             await asyncio.sleep(.1)
             while True:
