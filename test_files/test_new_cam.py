01272900398b8ae27eccd614977cbcd2a556757f
josefmoeggis <joeloe99@hotmail.com>
2024-12-03 17:51:00 +0100

    testing flexunpacking

--- test_files/test_new_cam.py
+++ test_files/test_new_cam.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 from time import sleep
-
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import FfmpegOutput
@@ -8,9 +7,14 @@ from picamera2.outputs import FfmpegOutput
 picam2 = Picamera2()
 picam2.configure(picam2.create_video_configuration(main={"size": (640, 480),
                                                          "format": "YUV420"}))
-picam2.start_recording(H264Encoder(), output=FfmpegOutput("-f rtp udp://127.0.0.1:9000"))
+
+# Use specific FFmpeg options to match the receiver
+output = FfmpegOutput("-f rtp -sdp_file video.sdp rtp://127.0.0.1:9000")
+
+picam2.start_recording(H264Encoder(), output=output)
+
 try:
     while True:
-        sleep(1)
+        sleep(1)  # Keep the script running
 except KeyboardInterrupt:
     picam2.stop_recording()
