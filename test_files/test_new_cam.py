61dace30b7a06f1a6084ce62959439c8ff546311
josefmoeggis <joeloe99@hotmail.com>
2024-12-03 18:05:28 +0100

    testing new cam3

--- test_files/test_new_cam.py
+++ test_files/test_new_cam.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
 from time import sleep
 from picamera2 import Picamera2
-from picamera2.encoders import H264Encoder
+from picamera2.encoders import MJPEGEncoder
 from picamera2.outputs import FfmpegOutput
 
 picam2 = Picamera2()
-picam2.configure(picam2.create_video_configuration(main={"size": (640, 480),
-                                                         "format": "YUV420"}))
+picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
 
-# Configure H264 encoder with specific parameters
-encoder = H264Encoder(bitrate=1000000, repeat=True, iperiod=15)
-
-# More robust FFmpeg output command
+# Configure MJPEG output
+encoder = MJPEGEncoder(bitrate=10000000)
 output = FfmpegOutput(
-    "-f h264 "
-    "-vcodec copy "  # Copy the encoded stream without re-encoding
-    "-tune zerolatency "  # Minimize latency
-    "-preset ultrafast "
-    "udp://10.22.46.50:9000"  # Your PC's IP
+    f"-f mpjpeg -r 30 udp://10.22.46.50:9000"  # Replace with your PC's IP
 )
 
 picam2.start_recording(encoder, output=output)
