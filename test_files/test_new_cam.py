5ec07aec53fe49e81f06b9ac3045a9a5dfad00d3
josefmoeggis <joeloe99@hotmail.com>
2024-12-03 17:59:32 +0100

    testing new cam

--- test_files/test_new_cam.py
+++ test_files/test_new_cam.py
@@ -8,8 +8,8 @@ picam2 = Picamera2()
 picam2.configure(picam2.create_video_configuration(main={"size": (640, 480),
                                                          "format": "YUV420"}))
 
-# Use the Windows PC's IP address instead of localhost
-output = FfmpegOutput("-f rtp -sdp_file video.sdp rtp://10.22.119.83:9000")  # Replace with your Windows PC's IP
+# Simpler UDP output format
+output = FfmpegOutput("-f h264 udp://10.22.119.83:9000")  # Replace with your PC's IP
 
 picam2.start_recording(H264Encoder(), output=output)
 
